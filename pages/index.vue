6babd2bb0ed6a4f773ef1fe47e09e08a4913086b
Celso Kenji Ishi <Kenji@GDBR.local>
2019-03-10 19:39:44 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>Nuxt Stripe Checkout</h1>
+      <br>
+      <button id="checkout-button" role="link">PAY</button>
+      <div id="error-message"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ plugins/stripe.js
@@ -0,0 +1,26 @@
+  var stripe = Stripe('pk_live_qH7pgkcC5bs2mXkLd9uuzAS0', {
+    betas: ['checkout_beta_4']
+  });
+
+  var checkoutButton = document.getElementById('checkout-button');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{sku: 'sku_Efq8ZMJ9xxusPn', quantity: 1}],
+
+      // Note that it is not guaranteed your customers will be redirected to this
+      // URL *100%* of the time, it's possible that they could e.g. close the
+      // tab between form submission and the redirect.
+      successUrl: 'https://upcare.online/success',
+      cancelUrl: 'https://upcare.online/canceled',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
