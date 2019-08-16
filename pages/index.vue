5bcc1537ab085b04b41e73e61f6880c18c31b220
Celso Kenji Ishi <Kenji@knjxz.local>
2019-08-15 21:16:53 -0300

    Ajustando home

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <section class="container">
     <div>
-      <h1>Nuxt Stripe Checkout</h1>
+      <div class="d-flex justify-content-center">
+            <img src="https://res.cloudinary.com/upcare-online/image/upload/v1548339178/Imagens/upcare_logo.png" alt="Responsive image">
+      </div>
+      <div class="d-flex justify-content-center">
+            <img src="https://res.cloudinary.com/upcare-online/image/upload/v1565913762/Imagens/nota.png" alt="Responsive image">
+      </div>
       <br>
-      <button id="checkout-button" role="link" class="btn btn-primary">PAGAR</button>
+      <h2>Taxa de indicação bem sucedida</h2>
+      <br>
+      <button id="checkout-button" role="link" class="btn btn-primary btn-lg">FAZER PAGAMENTO <i class="fas fa-chevron-right"></i></button>
       <div id="error-message"></div>
       </div>
     </div>
@@ -24,4 +31,12 @@ export default {
   align-items: center;
   text-align: center;
 }
+
+img {
+  width: 150px;
+  height: auto;
+  padding-top: 3%;
+  padding-bottom: 3%;
+}
+
 </style>
