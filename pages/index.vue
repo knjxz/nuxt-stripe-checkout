1b90ac21b8ae95ad2de3aabb2f5c6837670a4468
Celso Kenji Ishi <Kenji@knjxz.local>
2019-08-15 21:42:26 -0300

    Atualiando icon home

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,15 @@
       <div class="d-flex justify-content-center">
             <img src="https://res.cloudinary.com/upcare-online/image/upload/v1548339178/Imagens/upcare_logo.png" alt="Responsive image">
       </div>
-      <div class="d-flex justify-content-center">
-            <img src="https://res.cloudinary.com/upcare-online/image/upload/v1565913762/Imagens/nota.png" alt="Responsive image">
-      </div>
       <br>
+      <div class="d-flex justify-content-center">
+            <img src="https://res.cloudinary.com/upcare-online/image/upload/v1565915921/Imagens/indicacao-bem-sucedida.png" alt="Responsive image">
+      </div>
       <h2>Taxa de indicação bem sucedida</h2>
       <br>
       <button id="checkout-button" role="link" class="btn btn-primary btn-lg">FAZER PAGAMENTO <i class="fas fa-chevron-right"></i></button>
       <div id="error-message"></div>
+      <br>
       </div>
     </div>
   </section>
